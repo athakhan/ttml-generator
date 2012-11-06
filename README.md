98612b1f9429dd82f5c8626f81c82ffc42589327
Rodolfo Puig <Rodolfo.Puig@nbcuni.com>
2012-11-06 15:55:02 -0500

    Making use of the telemundo pypi package

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
-# TTML Generator #
+# TTML Generator
 
 Generates [TTML 1.0](http://www.w3.org/TR/ttaf1-dfxp/) files from different subtitle formats.
 
-## Usage ##
+## Usage
 
+    pip install -r requirements.txt
     python parse.py -h
 
-## Submitting bugs and feature requests ##
+## Requirements
 
-Bugs and feature request are tracked on [GitHub](https://github.com/telemundo/ttml-generator/issues)
+- telemundo 0.1
 
-## Author ##
+## Submitting bugs and feature requests
 
-Rodolfo Puig - <rodolfo.puig@nbcuni.com> - <https://twitter.com/rpuig_nbcuni><br />
+Bugs and feature request are tracked on [GitHub](https://github.com/telemundo/ttml-generator/issues).
 
-## License ##
+## Author
 
-TTML Generator is licensed under the MIT License - see the LICENSE file for details
+Rodolfo Puig - <rodolfo.puig@nbcuni.com> - [@rpuig_nbcuni](https://twitter.com/rpuig_nbcuni)
+
+## License
+
+This package is licensed under the MIT License - see the LICENSE file for details
--- parse.py
+++ parse.py
@@ -1,100 +1,44 @@
-'''
-This module generates TTML subtitle files from different formats.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-@author: Rodolfo Puig <Rodolfo.Puig@nbcuni.com>
-@copyright: Telemundo Digital Media
-@organization: NBCUniversal
-'''
-import os, re, time
+import os, time
 from optparse import OptionParser
-from xml.sax import saxutils
-from HTMLParser import HTMLParser
-from datetime import date
+from telemundo.subtitles import SubtitleParser
 
 parser = OptionParser(usage='usage: %prog [options] subtitle')
 parser.add_option('-v', action='count', dest='verbosity', default=0, help='increase output verbosity')
 parser.add_option('-q', '--quiet', action='store_true', dest='quiet', help='hide all output')
-parser.add_option('-l', '--lang', dest='lang', default='en-us', type='string', help='subtitle language (default: es)')
+parser.add_option('-l', '--lang', dest='lang', default='en-ES', type='string', help='subtitle language (default: en-ES)')
 parser.add_option('-f', '--file', dest='file', default=None, type='string', help='file where to store the TTML output')
 parser.add_option('-t', '--type', dest='type', default='xml', type='string', help='file extension')
 (options, args) = parser.parse_args()
 
-class CaptionParser(HTMLParser):
-    ''' Custom HTML Parser '''
-    stack = None
-    def __init__(self):
-        self.stack = []
-        self.reset()
-    def handle_data(self, data):
-        self.stack.append(saxutils.escape(data))
-    def render_output(self):
-        return ''.join(self.stack)
-
-def parse_srt_file(filename):
-    ''' Parse SRT subtitle file '''
-    lines = open(filename).read()
-    return re.findall(r'(?P<data>.*?)\r?\n\r?\n', lines, re.MULTILINE + re.DOTALL)
-
-def create_srt_caption(match):
-    ''' Create SRT subtitle dictionary '''
-    lines = match.splitlines()
-    return {
-        'pos': int(lines.pop(0)),
-        'timecode': map(lambda x: x.replace(',', '.'), lines.pop(0).split(' --> ')),
-        'text': '\n'.join(lines)
-    }
-
-def create_ttml_file(filename, subtitles):
-    ''' Create TTML subtitle file '''
-    fp = open(filename, 'wb')
-    fp.write('<?xml version="1.0" encoding="utf-8"?>\n')
-    fp.write('<tt xml:lang="%s" xmlns="http://www.w3.org/2006/10/ttaf1">\n' % options.lang)
-    fp.write('  <head>\n')
-    fp.write('    <metadata xmlns:ttm="http://www.w3.org/2006/10/ttaf1#metadata">\n')
-    fp.write('      <ttm:copyright>Telemundo (c) %s, all rights reserved.</ttm:copyright>\n' % date.today().year)
-    fp.write('    </metadata>\n')
-    fp.write('  </head>\n')
-    fp.write('  <body>\n')
-    fp.write('    <div>\n')
-    for subtitle in subtitles:
-        parser = CaptionParser()
-        parser.feed(subtitle['text'])
-        caption = parser.render_output()
-        line = '      <p xml:id="caption-%d" begin="%s" end="%s">%s</p>\n' % (subtitle['pos'], subtitle['timecode'][0], subtitle['timecode'][1], caption)
-        if options.verbosity >= 2 and not options.quiet:
-            print '[%s] DEBUG: %s' % (time.strftime('%Y-%m-%d %H:%M:%S'), saxutils.unescape(caption))
-        fp.write(line)
-    fp.write('    </div>\n')
-    fp.write('  </body>\n')
-    fp.write('</tt>\n')
-    fp.close()
-
 def main():
-    ''' Main routine '''
     if len(args) < 1:
         parser.error('you must specify a subtitle file to parse.')
+
     filename = args[0]
-    filepath = os.path.basename(filename)
-    filedir = os.path.dirname(filename)
-    (filebase, fileext) = os.path.splitext(filepath)
     if not os.path.exists(filename):
-        parser.error('the subtitle file %s does not exist.' % filename)
-    subtitles = []
-    matches = parse_srt_file(filename)
-    for match in matches:
-        subtitle = create_srt_caption(match)
-        if subtitle:
-            subtitles.append(subtitle)
-    if options.verbosity >= 1 and not options.quiet:
-        print '[%s] NOTICE: found %d subtitles' % (time.strftime('%Y-%m-%d %H:%M:%S'), len(subtitles))
-    if subtitles:
-        if options.file is not None:
-            ttmlfile = options.file
-        else:
-            ttmlfile = '%s_%s.%s' % (filebase, options.lang.upper(), options.type)
-        create_ttml_file('%s/%s' % (filedir, ttmlfile), subtitles)
+        parser.error('the subtitle file "%s" does not exist.' % filename)
+    
+    subparser = SubtitleParser()
+    captions = subparser.parse(filename, options.lang)
+
+    if len(captions) > 0:
         if options.verbosity >= 1 and not options.quiet:
-            print '[%s] NOTICE: %s was generated succesfully' % (time.strftime('%Y-%m-%d %H:%M:%S'), ttmlfile)
+            print '[%s] NOTICE: found %d subtitles' % (time.strftime('%Y-%m-%d %H:%M:%S'), len(captions))
+
+        if options.file is not None:
+            outputfile = options.file
+        else:
+            filepath = os.path.basename(filename)
+            filedir = os.path.dirname(filename)
+            (filebase, _) = os.path.splitext(filepath)
+            outputfile = '%s_%s.%s' % (filebase, options.lang.upper(), options.type)
+
+        captions.export('%s/%s' % (filedir, outputfile))
+        if options.verbosity >= 1 and not options.quiet:
+            print '[%s] NOTICE: %s was generated successfully' % (time.strftime('%Y-%m-%d %H:%M:%S'), outputfile)
     else:
         if options.verbosity >= 0 and not options.quiet:
             print '[%s] WARNING: no subtitles found' % (time.strftime('%Y-%m-%d %H:%M:%S'))
